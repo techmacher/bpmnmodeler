d22a769b5b61e8eb9b625f4df3cc2257686ceba6
SchuMacher <jagan@machers.tech>
2025-02-08 15:09:12 +0800

    feat: update sidebar to rename Chatbot to MacherBot and add icon

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -17,6 +17,7 @@ import {
 } from '@/components/ui/sidebar';
 import Link from 'next/link';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
+import { Bot } from 'lucide-react';
 
 export function AppSidebar({ user }: { user: User | undefined }) {
   const router = useRouter();
@@ -35,7 +36,7 @@ export function AppSidebar({ user }: { user: User | undefined }) {
               className="flex flex-row gap-3 items-center"
             >
               <span className="text-lg font-semibold px-2 hover:bg-muted rounded-md cursor-pointer">
-                Chatbot
+                MacherBot <Bot className="size-6" />
               </span>
             </Link>
             <Tooltip>
